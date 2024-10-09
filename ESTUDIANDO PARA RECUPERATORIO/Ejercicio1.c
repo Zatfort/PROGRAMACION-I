ea4f9f075c9dffcd0faf738f89e70bd4d4f3638e
daniel gutierrez <daniel_gutierrezn15@hotmail.com>
2024-10-09 15:56:17 -0300

    Actualizar impresión de notas en Ejercicio1.c y agregar Ejercicio3.c

--- ESTUDIANDO PARA RECUPERATORIO/Ejercicio1.c
+++ ESTUDIANDO PARA RECUPERATORIO/Ejercicio1.c
@@ -1,79 +1,53 @@
 #include <stdio.h>
+
 #include <string.h>
+
 #include <stdlib.h>
 
-int mostrarNotas(int cantidad,int array[]){
-    printf("sus notas ingresadas\n");
-    for (int i = 0; i<cantidad; i++)
-    {
-        printf("%d ",array[i]);
-    }
-    
-    return 0;
-}
 
-int cargarNotas(int array[]){
-    int numero;
+
+//ejercicio 1
+
+
+int cargarNotas(int arr[]){
+    int notas;
     int contador = 0;
-    printf("ingrese las notas del 1 a 10(0 para terminar): \n");
-    
-    while (numero != 0)
-    {
-        scanf("%d",&numero);
-        if (numero >= 1 && numero <= 10)
-        {
-            array[contador] = numero;
-            contador++;
-        }else if(numero == 0){
-            numero = 0;
-
-        }else{
-            printf("ingrese un numero valido \n");
+    while(notas != 0){
+        printf("Ingrese nota 1-10 (0 para finalizar)\n");
+        scanf("%d",&notas);
+        if(notas >= 1 && notas <= 10 ){
+                arr[contador]= notas;
+                contador++;
+        }else if(notas == 0){
+            printf("Fin de la carga. Las notas son: ");
+        }
+        else{
+            printf("Error. Ingrese nota 1-10 (0 para finalizar)\n");
         }
     }
-    return contador;
+return contador;
 }
 
-int main() {
-    int arr[50];
-    int cantidadNotas = cargarNotas(arr);
 
-    mostrarNotas(cantidadNotas,arr);
+int mostrarNotas(int cantidadNotas,int array[]){
+
+    for(int i = 0; i < cantidadNotas;i++){
+        printf("%d ",array[i]);
+    }
+
+
 
     return 0;
 }
 
+int main(){
+    int array[50];
+    int cantidadNotas;
+     cantidadNotas= cargarNotas(array);
+
+     mostrarNotas(cantidadNotas,array);
 
 
 
-
-
-
-
-
-
-
-
-
-
-/*
-    int array[50];  // Declarar un array de 50 elementos
-    int i = 0, num;
-
-    printf("Ingresa hasta 50 números enteros (ingresa 0 para terminar):\n");
-
-    // Ciclo para ingresar datos hasta 50 veces o hasta que se ingrese 0
-    while (num != 0) {
-        printf("Número %d: ", i + 1);
-        scanf("%d", &num);
-        array[i] = num;  // Guardar el número en el array
-        i++;
-    }
-
-    // Mostrar los números ingresados
-    printf("Has ingresado los siguientes números:\n");
-    for (int j = 0; j < i; j++) {
-        printf("%d ", array[j]);
-    }
-
-*/
+    return 0;
+}
---
+++ ESTUDIANDO PARA RECUPERATORIO/Ejercicio1conAyuda.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int mostrarNotas(int cantidad,int array[]){
+    printf("sus notas ingresadas\n");
+    for (int i = 0; i<cantidad; i++)
+    {
+        printf("%d ",array[i]);
+    }
+    
+    return 0;
+}
+
+int cargarNotas(int array[]){
+    int numero;
+    int contador = 0;
+    printf("ingrese las notas del 1 a 10(0 para terminar): \n");
+    
+    while (numero != 0)
+    {
+        scanf("%d",&numero);
+        if (numero >= 1 && numero <= 10)
+        {
+            array[contador] = numero;
+            contador++;
+        }else if(numero == 0){
+            numero = 0;
+
+        }else{
+            printf("ingrese un numero valido \n");
+        }
+    }
+    return contador;
+}
+
+int main() {
+    int arr[50];
+    int cantidadNotas = cargarNotas(arr);
+
+    mostrarNotas(cantidadNotas,arr);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    int array[50];  // Declarar un array de 50 elementos
+    int i = 0, num;
+
+    printf("Ingresa hasta 50 números enteros (ingresa 0 para terminar):\n");
+
+    // Ciclo para ingresar datos hasta 50 veces o hasta que se ingrese 0
+    while (num != 0) {
+        printf("Número %d: ", i + 1);
+        scanf("%d", &num);
+        array[i] = num;  // Guardar el número en el array
+        i++;
+    }
+
+    // Mostrar los números ingresados
+    printf("Has ingresado los siguientes números:\n");
+    for (int j = 0; j < i; j++) {
+        printf("%d ", array[j]);
+    }
+
+*/
